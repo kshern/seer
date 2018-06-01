2704ad94070267237434f0800dbea646d67f73a4
Shern <shern.shen@corp.netease.com>
2018-06-01 17:19:05 +0800

    静态页OK

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 
 const Koa = require('koa')
+const Static = require('koa-static');
+const path = require('path');
 
 let app = new Koa()
 
+const staticPath = './public/';
+
+app.use(Static(path.resolve(__dirname, staticPath)));
+
 app.use(async ctx => {
-    ctx.body = 'Hello World';
-  });
-  
-  app.listen(3000);
+  ctx.body = 'Hello World';
+});
+
+app.listen(3163);
